bcb627601fbc6ddca042badffba86b66ffc5296e
Mariia Okuneva <mokuneva@stat-econ.uni-kiel.de>
2022-10-13 17:45:04 +0200

    Fix 5792124.xml.
    Update the headline pattern for the case where one of the headlines
    starts from .\s and another one contains a period.

--- dpa code/split_articles.py
+++ dpa code/split_articles.py
@@ -611,8 +611,8 @@ def split_articles(multiple_articles):
                         headlines = re.findall(r'(?:^|(?<=\.\s{1})|(?<=\?\s{1})|(?<=\.»\s{1}))[^\.]+?(?=\(dpa(?!\)\.|\) [a-z]).+?)', txt)
                     
                     # If one of the headlines starts from .\s and another one contains a period.
-                    if len(headlines) < len(re.findall(r'(?:^|(?<=\.\n)|(?<=\?\n)|(?<=\.»\n)|(?<=\.\s{2}))[^\n]+?(?=\n.+?\(dpa.+?|\s{4}.+?\(dpa.+?)', row['texts'].strip())):
-                        headlines = re.findall(r'(?:^|(?<=\.\n)|(?<=\?\n)|(?<=\.»\n)|(?<=\.\s{2}))[^\n]+?(?=\n.+?\(dpa.+?|\s{4}.+?\(dpa.+?)', row['texts'].strip())
+                    if len(headlines) < len(re.findall(r'(?:^|(?<=\.\n)|(?<=\?\n)|(?<=\.»\n)|(?<=\.\s{2}))[^\n]+?(?:\n.+?\(dpa.+?|\s{4}.+?\(dpa.+?)', row['texts'].strip())):
+                        headlines = re.findall(r'(?:^|(?<=\.\n)|(?<=\?\n)|(?<=\.»\n)|(?<=\.\s{2}))[^\n]+?(?:\n.+?\(dpa.+?|\s{4}.+?\(dpa.+?)', row['texts'].strip())
                         headlines = [h.replace("\n", ' ').replace("\t", ' ').strip() for h in headlines]
                     
                     # Replace headline with 'SEP'
