5aae4a2711366cd8b081ab288e903f926ce2d11b
Mariia Okuneva <mokuneva@stat-econ.uni-kiel.de>
2022-10-23 13:29:31 +0200

    Fix 37185039.xml.
    Typo: .#+ => .

--- dpa code/split_articles.py
+++ dpa code/split_articles.py
@@ -75,7 +75,8 @@ def split_articles(multiple_articles):
         "Bodenobjekten.e": "Bodenobjekten.",
         "seien.m": "seien.",
         "Koalitionsgespräch.e": "Koalitionsgespräch.",
-        "weiterführen.d": "weiterführen."}
+        "weiterführen.d": "weiterführen.",
+        ".#+": "."}
 
         for typo, correction in typos_dic.items():
             row['texts'] = row['texts'].replace(typo, correction)  
