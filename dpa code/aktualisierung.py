41088f1a66bc28c3a9a2b830e20c41525fdbf25c
Jasper Bär <j.baer@stat-econ.uni-kiel.de>
2022-11-13 20:10:01 +0100

    Add the code for deleting outdated 'aktualisierung' articles
    Add the 'delete_aktualisierung_index' function which allows us to identify outdated 'aktualisierung' articles and delete them.

---
+++ dpa code/aktualisierung.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  2 23:30:07 2022
+
+@author: jbaer
+"""
+
+##############################################################################
+
+import re
+#import pandas as pd
+
+def delete_aktualisierung_index(data):
+    '''
+    This function identifies "Aktualisierung" articles and returns a list of indices
+    with outdated "Aktualisierung" articles to delete them later.
+    '''
+
+    delete_index = []
+    #keep_index = []
+    
+    for idx, title in enumerate(data['title']):
+        
+        # remove the "Aktualisierung" note from the title
+        title = re.sub('\((.*?)Aktualisierung(.*?)\)', '', title,1).strip()
+      
+        # check if title exists and is not part of "dpa-Vorausmeldungen"
+        if (len(title) > 1 and ('dpa-Vorausmeldungen kompakt: Das bringt der Tag.' and 'dpa-Vorausmeldung kompakt: Das bringt der Tag.') not in title):
+            
+            # select all articles with same title
+            akt_title = data[data['title'].str.contains(title, na = False, regex = False)]
+            
+            # if only one article with the given title was found, use the first
+            # sentence from the main text instead to select articles
+            if len(akt_title) == 1:
+                
+                text = data.iloc[idx]['texts']
+                text = re.sub('\((.*?)Aktualisierung(.*?)\)(\.?)', '', text,1).strip()
+                
+                # select the first sentence from the main text
+                try:
+                
+                    first_sent = re.findall('(^.*?[a-z]{2,}[.!?])\s+\W*[A-Z]', text)[0]
+                    
+                except:
+                    
+                    first_sent = ''
+                    
+                if  first_sent != '':
+                
+                    # select all articles with same starting sentence
+                    akt_sent = data[data['texts'].str.contains(first_sent, na = False, regex = False)]
+                    
+                    # if multiple article were found, save all indices except the last one
+                    # to delete the corresponding articles later
+                    if len(akt_sent) > 1:
+                        
+                        akt_ind = akt_sent.sort_values(by=['file']).index.tolist()
+                        delete_index.extend(akt_ind[:-1])
+                        
+                        #keep_index.extend(akt_ind[-1:]) 
+           
+            # if multiple article were found, save all indices except the last one
+            # to delete the corresponding articles later
+            elif len(akt_title) > 1:
+                
+                akt_ind = akt_title.sort_values(by=['file']).index.tolist()
+    
+                delete_index.extend(akt_ind[:-1])
+                #keep_index.extend(akt_ind[-1:])
+                
+    delete_index = list(set(delete_index))
+    #keep_index = list(set(delete_index))
+    
+    return(delete_index)
