e8f3193686cd0869bced9acc7b2fa07715a60b62
Mariia Okuneva <mokuneva@stat-econ.uni-kiel.de>
2022-11-22 18:35:03 +0100

    Add comments for the part on news updates.
    Search for updates within dpa data only.

--- dpa code/aktualisierung.py
+++ dpa code/aktualisierung.py
@@ -11,13 +11,23 @@ import re
 
 def delete_aktualisierung_index(data):
     '''
-    This function identifies "Aktualisierung" articles and returns a list of indices
-    with outdated "Aktualisierung" articles to delete them later.
+    This function identifies the updated news text/s with the keyword 'Aktualisierung'
+    in the title and its/their original version.
+    
+    If the original article and its update are published on the same day,
+    this function returns the index of the original article. Later, we use the index
+    to delete the original article. If multiple updates are published on the
+    same day, we only keep the one that was created last. To identify an update created
+    later than others, we use the file name.
+    
+    If the original article and its last update are published on different days,
+    both news reports are kept. However, if there is more than one update, we keep 
+    only the most recent one with the highest number in the file name. This function 
+    returns the indices of all intermediate updates that we later delete.
     '''
 
     delete_index = []
     ignore = []
-    #keep_index = []
     
     # select all "Aktualisierung" articles
     data_aktualisierung = data[data['title'].str.contains('\((?:.*?)Aktualisierung(?:.*?)\)', na = False)]
@@ -29,7 +39,7 @@ def delete_aktualisierung_index(data):
         # remove the "Aktualisierung" note from the title
         title = re.sub('\((?:.*?)Aktualisierung(?:.*?)\)', '', title,1).strip()
       
-        # check if title exists, is not on the ignore list and is not part of "dpa-Vorausmeldungen"
+        # check if a title exists, is not on the ignore list and is not part of "dpa-Vorausmeldungen"
         if (len(title) > 1 and title not in ignore and all(s not in title for s in ['dpa-Vorausmeldungen kompakt: Das bringt der Tag.', 'dpa-Vorausmeldung kompakt: Das bringt der Tag.'])):
             
             # add title to ignore list to ignore all articles with same title in the next iteration
@@ -38,8 +48,10 @@ def delete_aktualisierung_index(data):
             # select all articles with same title from the full dataset
             akt_title = data[data['title'].str.contains(title, na = False, regex = False)]
             
-            # if only one article with the given title was found, use the first
-            # sentence from the main text instead to select articles
+            # If only one article with the given title was found, use the first
+            # sentence from the main text instead to select articles.
+            # In most cases, the title and first sentence are the same, but
+            # sometimes the first sentence helps identify the original article.
             if len(akt_title) == 1:
                 
                 text = data_aktualisierung.iloc[idx]['texts']
@@ -59,19 +71,15 @@ def delete_aktualisierung_index(data):
                     # select all articles with same starting sentence from the full dataset
                     akt_sent = data[data['texts'].str.contains(first_sent, na = False, regex = False)]
                     
-                    # if multiple article were found, save all indices except the last one
-                    # to delete the corresponding articles later
+                    # if multiple articles were found, save indices of all the articles
                     if len(akt_sent) > 1:
                         
                         akt_ind = akt_sent.sort_values(by=['file']).index.tolist()                      
-                        #keep_index.extend(akt_ind[-1:]) 
            
-            # if multiple articles were found, save all indices except the last one
-            # to delete the corresponding articles later
+            # if multiple articles were found, save indices of all the articles
             elif len(akt_title) > 1:
                 
                 akt_ind = akt_title.sort_values(by=['file']).index.tolist()            
-                #keep_index.extend(akt_ind[-1:])
                 
             if akt_ind != []:
                  
@@ -81,8 +89,12 @@ def delete_aktualisierung_index(data):
                     data_first_art = data[data.index == akt_ind[0]][['day', 'month', 'year']].values.tolist()[0]
                     data_last_art = data[data.index == akt_ind[-1]][['day', 'month', 'year']].values.tolist()[0]
                     
-                    # check if the date of the original articles is the same as the latest "Aktualisierung" article 
-                    # add the index to delete the delete_index list if that is the case
+                    # Check if the date of the original article is the same as the date
+                    # of the latest "Aktualisierung" article.
+                    # Add the index of the original article and the indices of all 
+                    # the intermediate updates to the delete_index list if that is the case.
+                    # If that is not the case, keep the original article,
+                    # but delete all the intermediate updates.
                     if data_first_art == data_last_art:
                         
                         delete_index.extend(akt_ind[:-1])
@@ -96,6 +108,5 @@ def delete_aktualisierung_index(data):
                     delete_index.extend(akt_ind[:-1])
                 
     delete_index = list(set(delete_index))
-    #keep_index = list(set(delete_index))
     
     return(delete_index)
