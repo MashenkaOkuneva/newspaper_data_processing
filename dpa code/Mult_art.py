08f5529846405c5cf18f13af953639eab81b5e1f
Jasper Bär <j.baer@stat-econ.uni-kiel.de>
2023-01-04 18:25:37 +0100

    Update for the "Mult_art" code
    The following points were added.
    1. A distinction for articles which have their information for follow-up articles in the title instead of the main text
    2. Some smaller fixes and typos
    3. Additional commentary

--- dpa code/Mult_art.py
+++ dpa code/Mult_art.py
@@ -9,98 +9,179 @@ import pandas as pd
 from tqdm import tqdm
 import re
 
-#mult_art = data[data['title'].str.contains('(?:zwei|drei|drei|vier|fünf|sechs) Teile', na = False)]
+data = pd.read_csv('D:\Studium\PhD\Media Tenor\Results\Code neu\dpa_step1_testing.csv')
+mult_art = data[data['title'].str.contains('(?:zwei|drei|drei|vier|fünf|sechs) Teile', na = False)]
 
 def multi_part_art(mult_art, data):
+    '''
+    This function identifies texts which are split over multiple articles and 
+    merges them together to single articles.
+
+    '''
+    
+    def cont_art(title_fol, mult_art, delete_index, data, ty):    
+        
+        '''
+        This function searches for follow-up articles based on a given title
+        and returns text of the follow-up articles for further use.
+        
+        Parameters
+        ----------
+        title_fol : string or None
+            Title which is used to identify the following article
+        mult_art : DataFrame
+            Dataframe of articles which are potentially split up into smaller articles
+        delete_index : list
+            Indices of articles which can be deleted from main corpus. The function
+            adds the indices of the selected start and follow up articles.
+        data : DataFrame
+            Main Corpus
+        ty : string
+            Either "Type 1" or "Type 2". "Type 1" are articles where the last sentence is the 
+            beginning of the next article. "Type 2" are articles with the same main headline.
+
+        Returns
+        -------
+        text_fol : string
+            Text from the following article. The text
+            can be added to the text of the starting article.
+        delete_index : list
+            Indices of articles which can be deleted from the main corpus. 
+        title_fol : string or None
+            Title which can can be used to identify the next follow-up article in line.
+            
+        '''
+          
+        if len(title_fol) > 1:
+            
+            # Select all potential follow-up articles based on the title.
+            article_fol = data[data['title'].str.contains(title_fol, na = False, regex = False)]
+            
+            if not article_fol.empty:
+                
+                if len(article_fol) > 1:
+                
+                    # Get dates of all potential follow-up articles.
+                    date_fol = article_fol[['year', 'month', 'day']]
+                    
+                    # Get indices of all follow-up articles with the same date
+                    # as the first article.
+                    idx_fol = date_fol[(date_fol == date).all(1)]
+                    
+                    if not date_fol[(date_fol == date).all(1)].empty:
+                        
+                        if ty == "Type 1":
+                            
+                            idx_fol = idx_fol.index[0]
+                    
+                            text_fol = article_fol['texts'].iloc[0].replace(title_fol, '')
+                                
+                            # Get title for the next following article from the last sentence
+                            # of the article currently selected. 
+                            title_fol = re.search('Folgt (.{,75}?)$',text_fol)
+                            
+                            if title_fol is not None:
+  
+                                title_fol = title_fol.groups(1)[0]
+                            
+                        elif ty == "Type 2":
+                            
+                            idx_fol = list(idx_fol.index)
+                            
+                            article_fol = article_fol.loc[idx_fol]
+                            
+                            text_fol = ' '.join(article_fol['texts'])
+                            
+                            title_fol = None
+                        
+                    else:
+                        
+                        title_fol = None
+                        text_fol = ''
+                                            
+                else:
+                    
+                    text_fol = article_fol['texts'].iloc[0].replace(title_fol, '')
+                                  
+                    title_fol = re.search('Folgt (.{,75}?)$',text_fol)
+                    
+                    if title_fol is not None:
+  
+                        title_fol = title_fol.groups(1)[0]
+                
+                if ty == "Type 1":
+                    
+                    delete_index.append(article_fol.index[0])
+                    
+                elif ty == "Type 2":
+                    
+                    delete_index.extend(list(article_fol.index))
+                
+            else:
+                
+                title_fol = None
+                text_fol = '' 
+            
+        return(text_fol, delete_index, title_fol)
 
     delete_index = []
     articles_final = pd.DataFrame()
     
     for idx, title in tqdm(enumerate(mult_art['title'])):   
         
-        article = mult_art.iloc[idx].copy()
-        text = article['texts']
-        date = list(article[['year', 'month', 'day']])
+        if not mult_art.index[idx] in delete_index:
         
-        # If the last sentence of the article begins with "Folgt", it can be 
-        # used as a title to identify the following article
-        
-        title_fol = re.search('Folgt (.{,75}?)$',text)
-        
-        if title_fol is not None:
+            article = mult_art.iloc[idx].copy()
+            text = article['texts']            
+            date = list(article[['year', 'month', 'day']])
             
-            title_fol = title_fol.groups(1)[0]
-        
-        def cont_art(title_fol, mult_art, delete_index, data):
+            delete_index.append(mult_art.index[idx])
             
+            # If the last sentence of the article begins with "Folgt", it can be 
+            # used as a title to identify the following article.
+            title_fol = re.search('[Ff]olgt (.{,75}?)(?:\)|$|(?: # dpa-Notizblock)|(?:\())',text)
             
-            if len(title_fol) > 1:
+            if title_fol is not None:
                 
-                # Select all potential follow-up articles based on the title
-                article_fol = data[data['title'].str.contains(title_fol, na = False, regex = False)]
+                title_fol = title_fol.groups(1)[0]
+                text = re.sub('Folgt ' + title_fol, '', text)
                 
-                if not article_fol.empty:
+                ty = "Type 1"
                 
-                    # If multiple potential follow-up articles where found, select
-                    # only the article with the same date as the first article
-                    if not (isinstance(article_fol, pd.Series)):
-                        
-                        article_fol.reset_index(inplace = True, drop = True)
-                    
-                        date_fol = article_fol[['year', 'month', 'day']]
-                        
-                        idx_fol = date_fol[(date_fol == date).all(1)]
-                        
-                        if not date_fol[(date_fol == date).all(1)].empty:
-                        
-                            idx_fol = idx_fol.index[0]
-                        
-                            article_fol = article_fol.iloc[idx_fol]
-                    
-                            text_fol = re.sub(title_fol, '', article_fol['texts'])
-                                      
-                            title_fol = re.search('Folgt (.{,75}?)$',text_fol)
-                            
-                        else:
-                            
-                            title_fol = None
-                            text_fol = ''
-                    
-                    if title_fol is not None:
-                    
-                        title_fol = title_fol.groups(1)[0]
-                        
-                    delete_index.append(article_fol.index[0])
-                    
-                else:
-                    
-                    title_fol = None
-                    text_fol = '' 
+            elif title_fol is None:
                 
-            return(text_fol, delete_index, title_fol)
+                # If the title of the articles includes "Teil (number) - (number) Teile"
+                # (part (number) - (number) parts) the title can be used to identify 
+                # all articles which belong together.
+                
+                ty = "Type 2"
+                
+                try:
+                    
+                    title_fol = re.search('(- \w+ Teile)(.*)', title).groups(0)[1]
+                                   
+                except:
+                    
+                    continue
+            
+            if ty == "Type 1":
+            
+                # Identfiy the followign article and add its text to the text of the starting article.
+                # Repeat this step until the last article is reached.
+                while title_fol is not None:
+                    
+                    text_fol, delete_index, title_fol = cont_art(title_fol, mult_art, delete_index, data, ty)
+                        
+                    text += text_fol
+                    
+            elif ty == "Type 2":
+                
+                text, delete_index, title_fol = cont_art(title_fol, mult_art, delete_index, data, ty)
+                
+            article.loc['texts'] = text
+            
+            articles_final = articles_final.append(article)
+         
+    delete_index = list(set(delete_index))    
     
-        # Use the last sentence from each article to search for the following article. 
-        while title_fol is not None:
-            
-            text_fol, delete_index, title_fol = cont_art(title_fol, mult_art, delete_index, data)
-                
-            text += text_fol
-                
-        article.loc['texts'] = text
-        
-        articles_final = articles_final.append(article)
-        
-    return(delete_index)
-        
-multi_part_art(mult_art)          
-
-
-# Examples:
-# 5739309.xml
-# 5739311.xml
-
-
-# 5739731.xml
-
-# keyword:  Abendvorschau   
-# title: Die 603 Abgeordneten des Bundestages in der 15. Wahlperiode.   
+    return(delete_index, articles_final)
